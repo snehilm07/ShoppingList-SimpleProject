ebf31a6d695b4bb74cd6bf6d26de223986294261
snehilm07 <snehilm07@gmail.com>
2024-01-13 11:15:55 +0530

    Added ViewModel Factory and ViewModel

--- app/src/main/java/com/quickandroidapps/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/quickandroidapps/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,11 +2,20 @@ package com.quickandroidapps.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.quickandroidapps.shoppinglist.R
+import com.quickandroidapps.shoppinglist.data.db.ShoppingDatabase
+import com.quickandroidapps.shoppinglist.data.repository.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProviders.of(this, factory)[ShoppingViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/quickandroidapps/shoppinglist/ui/shoppinglist/ShoppingViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.quickandroidapps.shoppinglist.ui.shoppinglist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.quickandroidapps.shoppinglist.data.repository.ShoppingRepository
+
+@Suppress("UNCHECKED_CAST")
+class ShoppingViewModelFactory(
+    private val repository: ShoppingRepository
+): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ShoppingViewModel(repository) as Tgit
+    }
+
+}
